0a70cc473556c8437686cd24e9ad0d252776d867
Guido <guidoalloatti@gmail.com>
2012-08-16 18:24:22 -0300

    March me apura

--- static/js/sounds.js
+++ static/js/sounds.js
@@ -11,23 +11,22 @@ function soundSwitcher()
 	if(!soundOn) {
 		$("#soundImage").html('<a  onclick="soundSwitcher();"><img src="images/check.png" alt="Sound is OFF" width="32" height="32" border="0" /></a>');
 		$("#soundStatus").html("Sound is OFF");
-        $("#log").text($("#log").text()+"\nSound is OFF");
+		$("#log").text($("#log").text()+"\nSound is OFF");
 	} else {
-        $("#soundImage").html('<a onclick="soundSwitcher();"><img src="images/uncheck.png" alt="Sound is OON" width="32" height="32" border="0" /></a>');
+		 $("#soundImage").html('<a onclick="soundSwitcher();"><img src="images/uncheck.png" alt="Sound is ON" width="32" height="32" border="0" /></a>');
 		$("#soundStatus").html("Sound is ON");
-        $("#log").text($("#log").text()+"\nSound is ON");
+		$("#log").text($("#log").text()+"\nSound is ON");
 	}
 }
 
 function pauseSwitcher()
 {
 	pauseOn = !pauseOn;
-
 	if(!pauseOn) {
 		$("#pauseImage").html('<a  onclick="pauseSwitcher();"><img src="images/check.png" alt="Pause is OFF" width="32" height="32" border="0" /></a>');
 		$("#pauseStatus").html("Pause is OFF");
-        $("#log").text($("#log").text()+"\nPause is OFF");
-        pause();
+		$("#log").text($("#log").text()+"\nPause is OFF");
+		pause();
 	} else {
 		$("#pauseImage").html('<a onclick="pauseSwitcher();"><img src="images/uncheck.png" alt="Pause is ON" width="32" height="32" border="0" /></a>');
 		$("#pauseStatus").html("Pause is ON");
